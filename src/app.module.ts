cd88a7ae40c48972d80c9ef9f934fbe1dff6fb36
luan <los8@aluno.ifal.edu.br>
2023-09-19 16:27:39 -0300

    1234 senha

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-ck4ucpmi9prc73agni90-a.oregon-postgres.render.com',
+      port: 3306,
+      username: 'projeto_nestjs_typescript_sql_user',
+      password: '1234',
+      database: 'projeto_nestjs_typescript_sql',
+      entities: [],
+      synchronize: true,
+      ssl: true
+    })
+  ],
+  
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,4 +1,4 @@
-import { Module, Controller } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
